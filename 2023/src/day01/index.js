8adc72594b2c0c292f94609cce4777f02d5e2264
Erik Kieckhafer <ek@ato.la>
2023-12-04 17:07:15 -0700

    day 1, part 1 solution
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- 2023/src/day01/index.js
+++ 2023/src/day01/index.js
@@ -1,11 +1,26 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput) => rawInput;
+const parseInput = (rawInput) => rawInput.split("\n").map((a) => a);
 
 const part1 = (rawInput) => {
+
   const input = parseInput(rawInput);
 
-  return;
+  // remove all non-numeric characters
+  const numericValues = input.map((value) => value.replace(/\D/g,''));
+
+  // get first and last digit, and convert to a number
+  const firstAndLastValues = numericValues.map((value) => {
+    const first = value.slice(0,1);
+    const second = value.slice(-1);
+    const combined = first + second;
+    return Number(combined);
+  });
+
+  // add all values together
+  const totalSum = firstAndLastValues.reduce((partialSum, a) => partialSum + a, 0);
+
+  return totalSum;
 };
 
 const part2 = (rawInput) => {
